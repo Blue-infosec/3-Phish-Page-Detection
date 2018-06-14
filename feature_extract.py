1a38853b834ac4636d70c403e57d030ffe5f9cef
KeTian <ririhedou@gmail.com>
2018-06-14 15:08:16 -0400

    up

--- feature_extract.py
+++ feature_extract.py
@@ -152,26 +152,26 @@ def text_embedding_into_vector(txt_str):
     return embedding_vector
 
 
-def feature_vector_extraction(c):
+def feature_vector_extraction(candidate):
     """
-    :param c: a candidate object
+    :param candidate: a candidate object
     :return: the feature vector
     it consists of three components: img-text, html-text, form-text
     """
     print ("Analyse source and image at:")
-    print (c.source_html)
-    print (c.img_path)
+    print (candidate.source_html)
+    print (candidate.img_path)
 
-    if os.path.exists(c.source_html) and os.path.exists(c.img_path):
+    if os.path.exists(candidate.source_html) and os.path.exists(candidate.img_path):
         try:
-            img_text = get_img_text_ocr(c.img_path)
+            img_text = get_img_text_ocr(candidate.img_path)
 
             #print (img_text)
 
             if len(img_text) == 0:
                 return None
 
-            text_word_str, num_of_forms, attr_word_str = get_structure_html_text(c.source_html)
+            text_word_str, num_of_forms, attr_word_str = get_structure_html_text(candidate.source_html)
             img_v = text_embedding_into_vector(img_text)
             txt_v = text_embedding_into_vector(text_word_str)
             form_v = text_embedding_into_vector(attr_word_str)
